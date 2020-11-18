92926b7d56acfee9b3b536de9b0e6c38150cab6b
francesco marinelli <francescomarinelli04@gmail.com>
2020-11-18 18:33:35 +0100

    Aggiungere i file di progetto.

---
+++ ASCII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci carattere ASCII");
+            char caratteredec = Convert.ToChar(Console.ReadLine());
+            ushort carattereASCII = 0;
+            carattereASCII = caratteredec;
+            string num1 = Convert.ToString(carattereASCII, 2).PadLeft(16, '0');
+            Console.WriteLine($"Carattere {caratteredec} - Carattere in ASCII {carattereASCII} - NumeroBinario {num1}");
+            Console.ReadLine();
+        }
+    }
+}
